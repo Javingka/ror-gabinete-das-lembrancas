bb61d2b943b061143e3527bd74bd789c96a7f6df
Javier Cruz <jcruzsm@gmail.com>
2015-03-19 23:06:07 -0300

    problema subiendo en heroku intento 1

--- app/assets/javascripts/menu/main.js
+++ app/assets/javascripts/menu/main.js
@@ -1,11 +1,13 @@
 ;(function(){
+	// Menu settings
+	$('#menuToggle, .menu-close').on('click', function(){
+		$('#menuToggle').toggleClass('active');
+		$('body').toggleClass('body-push-toleft');
+		$('#theMenu').toggleClass('menu-open');
+	});
 
-			// Menu settings
-			$('#menuToggle, .menu-close').on('click', function(){
-				$('#menuToggle').toggleClass('active');
-				$('body').toggleClass('body-push-toleft');
-				$('#theMenu').toggleClass('menu-open');
-			});
+	$('#menuToggle').on('click', function(){
+		$('.text-vertical-about').css('background-color:#000;')
 
-
+	});
 })(jQuery)
